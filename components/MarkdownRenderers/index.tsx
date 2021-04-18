d9dceae1e5f4d1b07c49b407721440b7eb891643
SGarcia710 <sgarcia710@outlook.com>
2021-04-17 19:50:59 -0500

    Now MD renderers are separated

---
+++ components/MarkdownRenderers/index.tsx
@@ -0,0 +1,44 @@
+import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
+
+// import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+// import {
+//   dark,
+//   synthwave84,
+//   dracula,
+// } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+// https://github.com/react-syntax-highlighter/react-syntax-highlighter/issues/230
+
+import Code from './Code';
+import Table from './Table';
+
+const components = {
+  table: Table,
+  code: ({ node, className, ...props }) => {
+    const match = /language-(\w+)/.exec(className || '');
+    return match ? (
+      // <SyntaxHighlighter
+      //   language={match[1]}
+      //   PreTag="div"
+      //   style={dracula}
+      //   {...props}
+      // />
+      <Code codeString={props.children.shift()} language={match[1]} />
+    ) : (
+      <code className={className} {...props} />
+    );
+  },
+  wrapper: ({ children }) => <>{children}</>,
+};
+
+const MarkdownRenderer = (props: { content: string }) => {
+  return (
+    <ReactMarkdown
+      children={props.content}
+      components={components}
+      remarkPlugins={[gfm]}
+    />
+  );
+};
+
+export default MarkdownRenderer;
