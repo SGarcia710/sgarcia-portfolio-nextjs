20eb64c1620bedd975efdba4f8d7e1c1da123e93
Sebastian Garcia <sgarcia710@outlook.com>
2021-07-04 09:07:25 -0500

    Configured Favicon for all devices

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,10 +8,34 @@ import GlobalStyles from '@/assets/styles/Global';
 import Layout from '@/components/Layout';
 import { COLORS } from '@/constants';
 import ScrollToTop from '@/components/ScrollToTop';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#1A1A1A" />
+      </Head>
       <GlobalStyles />
       <Layout>
         <NextNprogress
